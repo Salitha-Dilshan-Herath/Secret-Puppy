7ae002d4d1fa58a81d520d31f651e0f759cd47e9
Salitha <salith.2017006@iit.ac.lk>
2020-02-23 10:26:23 +0530

    add identifiyBeeddActivity

--- app/src/main/java/com/iit/secretpuppy/MainActivity.java
+++ app/src/main/java/com/iit/secretpuppy/MainActivity.java
@@ -2,13 +2,35 @@ package com.iit.secretpuppy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnBreed;
+    private Button btnDog;
+    private Button btnDogSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnBreed     = findViewById(R.id.btnBreed);
+        btnDog       = findViewById(R.id.btnDog);
+        btnDogSearch = findViewById(R.id.btnDogSearch);
+
+        btnBreed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(),IdentifyBreedActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
